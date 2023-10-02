f7b5f2de130b42562da4be3cbf2a853175acdcc4
MarianoBarella95 <marianobarella1@gmail.com>
2023-10-02 16:32:35 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const num1 = document.querySelector('.number1');
+const num2 = document.querySelector('.number2');
+const btnSumar = document.querySelector('.btn-sumar');
+const btnMultiplicar = document.querySelector('.btn-multiplicar');
+const btnRestar = document.querySelector('.btn-restar');
+const btnDividir = document.querySelector('.btn-dividir');
+const divResultado = document.querySelector('.resultado')
+const btnReset = document.querySelector('.btn-reset');
+
+
+btnSumar.addEventListener('click', () => {
+    let resultado = num1.valueAsNumber + num2.valueAsNumber
+    divResultado.innerHTML = resultado;
+}) 
+
+btnRestar.addEventListener('click', () => {
+    let resultado = num1.valueAsNumber - num2.valueAsNumber
+    divResultado.innerHTML = resultado;
+})
+
+btnMultiplicar.addEventListener('click', () => {
+    let resultado = num1.valueAsNumber * num2.valueAsNumber
+    divResultado.innerHTML = resultado;
+})
+
+btnDividir.addEventListener('click', () => {
+    let resultado = num1.valueAsNumber / num2.valueAsNumber
+    divResultado.innerHTML = resultado;
+})
+
+btnReset.addEventListener('click', () => {
+    divResultado.innerHTML = '';
+    num1.value = '';
+    num2.value = '';
+})
+
